9a9c41f656acbc8257b5e1e735672b1f20c489df
Mohammad Qais Khan <qaisphone2@gmail.com>
2026-03-25 22:08:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import TodoList from './pages/utilities/TodoList'
 import PasswordGenerator from './pages/utilities/PasswordGenerator'
 import BMICalculator from './pages/utilities/BMICalculator'
 import UnitConverter from './pages/utilities/UnitConverter'
-// import NotePad from './pages/utilities/NotePad'
+import NotePad from './pages/utilities/NotePad'
 // import ColorPicker from './pages/utilities/ColorPicker'
 // import StopWatch from './pages/utilities/StopWatch'
 // import AgeCalculator from './pages/utilities/AgeCalculator'
@@ -53,6 +53,9 @@ const App = () => {
           <Route path= "utility/unit-converter" element={
             <ProtectedRoute><UnitConverter/></ProtectedRoute>
           }/>
+          <Route path="/utility/notepad" element={
+            <ProtectedRoute><NotePad /></ProtectedRoute>
+          } />
           {/* 
           <Route path="/utility/notepad" element={
             <ProtectedRoute><NotePad /></ProtectedRoute>
