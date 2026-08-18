809fa43d58c6a80a537458149aeee8c6badf8327
Mohammad Qais Khan <qaisphone2@gmail.com>
2026-08-18 20:23:09 +0530

    Added Lazy Loading to core pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // src/App.jsx
+import { Suspense, lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
