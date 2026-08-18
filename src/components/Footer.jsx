c3ee2d8c8b2fd6eb9241b9bd72da749f65bf37c5
Mohammad Qais Khan <qaisphone2@gmail.com>
2026-08-18 20:41:31 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,6 @@ const Footer = () => {
     GitHub: "https://github.com/qaiiisgit",
     LinkedIn: "https://linkedin.com/in/md-qais-khan"
   };
-<<<<<<< HEAD
-
-=======
->>>>>>> 731852c (Update Footer and README)
   return (
     <footer className="bg-gray-900 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
